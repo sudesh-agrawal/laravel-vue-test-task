fd7fe0aed836e88da87173591afb9f405f5bf664
rameshwardev <rameshwar.dhakad@vkaps.com>
2022-08-19 11:05:09 +0530

    routing set

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any','.*');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
